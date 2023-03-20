597d837b948ab9388cfdc63e33bcfd287be46298
Kenny4571 <119124617+Kenny4571@users.noreply.github.com>
2023-03-20 12:27:09 -0500

    Delete nameSort.kt

--- nameSort.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    print("Charlie, Leslie, Andy:")
-    val inputStrings = ("Andy, Charlie, Leslie")
-    val sortedStrings = inputStrings    
-    
-    println("The sorted names are: ")
-    sortedStrings.forEach { println(it) }
-    
-}
